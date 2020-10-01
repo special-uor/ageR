8cbdfa5e468f462e5fb715035365c416bebce719
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-01 18:01:00 +0100

    Updating example workflow

--- R.ignore/pipeline.R
+++ R.ignore/pipeline.R
@@ -2,6 +2,6 @@ is_valid_connection <- function(conn) {
   return(class(conn) == "MariaDBConnection")
 }
 
-conn <- open_conn_mysql("RPD-latest", "root")
-out <- select_query_mysql(conn, "SELECT * FROM `RPD-latest`.date_info LIMIT 10;")
-close_conn_mysql(conn)
+conn <- ageR::open_conn_mysql("RPD-latest", "root")
+out <- ageR::select_query_mysql(conn, "SELECT * FROM `RPD-latest`.date_info LIMIT 10;")
+ageR::close_conn_mysql(conn)
