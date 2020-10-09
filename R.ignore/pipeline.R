66ffb43508fb5c195e16819eafeccabb942189e8
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-09 20:51:10 +0100

    Cleaning example pipeline

--- R.ignore/pipeline.R
+++ R.ignore/pipeline.R
@@ -1,36 +1,9 @@
-# library(ageR)
-wdir <- here::here("runs/002")
-# prefix <- "rpd001"
+wdir <- here::here("runs/006")
+tictoc::tic("Retrive data from DB")
 `%>%` <- dplyr::`%>%`
 dir.create(wdir, showWarnings = FALSE, recursive = FALSE)
 
-# dir.create(file.path(wdir, 'Bacon_runs/'),
-#            showWarnings = FALSE,
-#            recursive = TRUE)
-# setwd(file.path(wdir, 'Bacon_runs/'))
 conn <- ageR::open_conn_mysql("RPD-latest", "root")
-# out <- ageR::select_query_mysql(conn, "SELECT * FROM `RPD-latest`.date_info LIMIT 10;")
-# query <- paste0(
-# "SELECT `RPD-latest`.date_info.ID_ENTITY as ID_ENTITY,
-#        age_C14,
-#        error,
-#        avg_depth,
-#        `RPD-latest`.entity.ID_ENTITY AS ID_ENTITY,
-#        entity_name AS ENTITY,
-#        `RPD-latest`.site.ID_SITE AS ID_SITE,
-#        site_name AS SITE
-# FROM
-# 	(`RPD-latest`.date_info INNER JOIN `RPD-latest`.entity
-# 		ON `RPD-latest`.date_info.ID_ENTITY = `RPD-latest`.entity.ID_ENTITY)
-# INNER JOIN `RPD-latest`.site
-# 			ON `RPD-latest`.entity.ID_SITE = `RPD-latest`.site.ID_SITE")
-# query <- paste0(
-# "SELECT lab_number AS labID,
-#        age_C14 AS age,
-#        error,
-#        avg_depth AS depth
-# FROM date_info
-# WHERE material_dated = 'charcoal'")
 query <- paste0(
 "SELECT entity_name,
        entity.ID_ENTITY as entity_id,
@@ -54,11 +27,6 @@ rpd <- rpd %>%
   dplyr::mutate(cc = ifelse(grepl("*carbon", date_type), 1, 0))
 rpd$latitude <- NULL
 
-# query <- "SELECT ID_SAMPLE AS id, sample_depth AS depth FROM sample"
-# sample_tb <- ageR::select_query_mysql(conn, query)
-# sample_tb <- sample_tb %>%
-#   dplyr::filter(depth != -9999)
-
 entities <- sort(unique(rpd$entity_name))
 entities_id <- c()
 entities_clean_names <- c()
@@ -69,8 +37,9 @@ pb <- progress::progress_bar$new(
 for (entity in entities) {
   pb$tick()
   nent <- iconv(entity, to = 'ASCII//TRANSLIT') # Remove accented characters
-  nent <- gsub("[[:punct:]]", "", nent)
-  nent <- gsub(" ", "-", nent)
+  # nent <- gsub("[[:punct:]]", "", nent)
+  nent <- gsub("(?![_-])[[:punct:]]", "", nent, perl = TRUE)
+  # nent <- gsub(" ", "-", nent)
   entities_clean_names <- c(entities_clean_names, nent)
   idx <- rpd$entity_name == entity
   entities_id <- c(entities_id, rpd[idx, 2][1])
@@ -87,7 +56,7 @@ for (entity in entities) {
              showWarnings = FALSE,
              recursive = TRUE)
   setwd(file.path(wdir, nent, "core"))
-  write.csv(rpd[idx, -c(1:2)], paste0(nent, ".csv"), row.names = FALSE)
+  # write.csv(rpd[idx, -c(1:2)], paste0(nent, ".csv"), row.names = FALSE)
   write.csv(sample_tb, paste0(nent, "_depths.csv"), row.names = FALSE)
   write.table(sample_tb$depth,
               paste0(nent, "_depths.txt"),
@@ -98,10 +67,11 @@ for (entity in entities) {
             row.names = FALSE)
 
   # Data for the Bacon model
-  dir.create(file.path(wdir, nent, '/Bacon_runs/', nent),
+  dir.create(file.path(wdir, nent, 'Bacon_runs', nent),
              showWarnings = FALSE,
              recursive = TRUE)
-  setwd(file.path(wdir, nent, '/Bacon_runs/', nent))
+  setwd(file.path(wdir, nent, 'Bacon_runs', nent))
+  # c("labID", "age", "error", "depth", "cc")
   write.csv(rpd[idx, c("labID", "age", "error", "depth", "cc")],
             paste0(nent, ".csv"), row.names = FALSE)
   . <- file.symlink(from = paste0("../../core/", nent, "_depths.txt"),
@@ -109,19 +79,19 @@ for (entity in entities) {
   . <- file.symlink(from = paste0("../../core/", nent, "_sample_ids.csv"),
                     to = paste0("."))
   # Empty data frame for "not used dates"
-  setwd(file.path(wdir))
+  setwd(file.path(wdir, nent))
   not_used_dates <- data.frame(depth = NA, error = NA)[-1, ]
   write.csv(not_used_dates, "not_used_dates.csv", row.names = FALSE)
 
   # Empty data frame for Hiatus
-  hiatus_tb <- data.frame(sample_id = NA, depth_sample_bacon = NA)[-1, ]
+  hiatus_tb <- data.frame(sample_id = NA, depth_sample = NA)[-1, ]
   write.csv(hiatus_tb, "hiatus.csv", row.names = FALSE)
 
   # Data for the Bchron model
-  dir.create(file.path(wdir, nent, '/Bchron/'),
+  dir.create(file.path(wdir, nent, 'Bchron'),
              showWarnings = FALSE,
              recursive = TRUE)
-  setwd(file.path(wdir, nent, '/Bchron/'))
+  setwd(file.path(wdir, nent, 'Bchron'))
   # c("labID", "age", "error", "depth", "thickness", "calib_used")
   # mutate(calib_curve_new = case_when(
   #   calib_used == 'NULL' ~ 'normal',
@@ -135,30 +105,32 @@ for (entity in entities) {
 
 
   # Data for the Linear Interpolation model
-  dir.create(file.path(wdir, nent, '/linInterp/'),
+  dir.create(file.path(wdir, nent, 'linInterp'),
              showWarnings = FALSE,
              recursive = TRUE)
-  setwd(file.path(wdir, nent, '/linInterp/'))
+  setwd(file.path(wdir, nent, 'linInterp'))
   write.csv(rpd[idx, c("labID", "age", "error", "depth", "date_type")],
             paste0(nent, ".csv"), row.names = FALSE)
   . <- file.symlink(from = paste0("../core/", nent, "_depths.csv"),
                     to = paste0("."))
 
   # Data for the Linear Regression model
-  dir.create(file.path(wdir, nent, '/linReg/'),
+  dir.create(file.path(wdir, nent, 'linReg'),
              showWarnings = FALSE,
              recursive = TRUE)
-  setwd(file.path(wdir, nent, '/linReg/'))
+  setwd(file.path(wdir, nent, 'linReg'))
   write.csv(rpd[idx, c("labID", "age", "error", "depth", "date_type")],
             paste0(nent, ".csv"), row.names = FALSE)
   . <- file.symlink(from = paste0("../core/", nent, "_depths.csv"),
                     to = paste0("."))
+  . <- file.symlink(from = paste0("../core/", nent, "_depths.csv"),
+                    to = paste0("./", nent, "_ids.csv"))
 
   # Data for the StalAge model
-  dir.create(file.path(wdir, nent, '/StalAge/'),
+  dir.create(file.path(wdir, nent, 'StalAge'),
              showWarnings = FALSE,
              recursive = TRUE)
-  setwd(file.path(wdir, nent, '/StalAge/'))
+  setwd(file.path(wdir, nent, 'StalAge'))
   write.csv(rpd[idx, c("labID", "age", "error", "depth")],
             paste0(nent, ".csv"), row.names = FALSE)
   . <- file.symlink(from = paste0("../core/", nent, "_depths.csv"),
@@ -172,6 +144,16 @@ write.csv(entities, "entities.csv", row.names = FALSE)
 
 # charcoal <- ageR::select_all(conn, table = "charcoal")
 ageR::close_conn(conn)
+tictoc::toc()
 
-# ageR::runBacon(wdir, entities_clean_names[1])
-# ageR::runBacon(wdir, entities_clean_names[2])
+# wdir <- here::here("runs/004")
+# setwd(wdir)
+entities <- read.csv("entities.csv")
+AM <- function(i) {
+  tictoc::tic(paste0(entities$original[i]))
+  setwd(wdir)
+  ageR::runBacon(wdir, entities$clean[i])
+  # ageR::runLinReg(wdir, entities$clean[i])
+  tictoc::toc()
+}
+AM(1)
