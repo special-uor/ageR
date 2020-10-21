bfaf64f836c541738a8c4d28266b5e66da73346d
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-21 14:24:56 +0100

    Adding lower and upper bound parameters, to constraint the sequence

--- R/utils-scenarios.R
+++ R/utils-scenarios.R
@@ -5,15 +5,22 @@
 #'
 #' @param ref Reference value.
 #' @param step Size of increments.
+#' @param lower Lower bound. If not passed, it will default to \code{ref / 2}.
+#' @param upper Upper bound. If not passed, it will default to \code{2 * ref}.
 #'
 #' @return Evenly spaced array.
 #'
 #' @examples
-#' sce_seq(50)
+#' ageR:::sce_seq(50)
+#' ageR:::sce_seq(50, lower = 30, upper = 80)
 #'
 #' @keywords internal
 #' @noRd
-sce_seq <- function(ref, step = 5) {
-  return(unique(c(seq(floor(ref / 2), ref, by = step),
-                  seq(ref, ceiling(2 * ref), by = 2 * step))))
+sce_seq <- function(ref, step = 5, lower = NULL, upper = NULL) {
+  if (is.null(lower))
+    lower <- floor(ref / 2)
+  if (is.null(upper))
+    upper <- ceiling(2 * ref)
+  return(unique(c(seq(lower, ref, by = step),
+                  seq(ref, upper, by = 2 * step))))
 }
