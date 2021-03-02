966a386dccd6a2d1c11127273ff6429960724808
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-03-02 13:43:12 +0000

    Add function to display a progress bar, pb

--- R/utils-scenarios.R
+++ R/utils-scenarios.R
@@ -50,3 +50,18 @@ is.done <- function(wdir, entity) {
     return(TRUE)
   return(FALSE)
 }
+
+#' Show progress bar
+#'
+#' @param expr R expression.
+#' @inheritDotParams progressr::with_progress -handlers
+#'
+#' @return Return data from the function called.
+#' @export
+pb <- function(expr, ...) {
+  progress_bar <-
+    progressr::handler_progress(format = "(:current/:total) [:bar] :percent")
+  progressr::with_progress(expr,
+                           ...,
+                           handlers = progress_bar)
+}
